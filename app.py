95a3ac9debc646e1c07639e76694dccd520a8187
jerinuser <jerinarockiadass2907@gmail.com>
2024-09-03 22:52:31 +0530

    versel file added

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
-    data = request.form['field_name']
-    # Process data
-    return 'Data submitted!'
+def welcome():
+    return 'Welcome'
 
 if __name__ == '__main__':
     app.run(debug=True)
